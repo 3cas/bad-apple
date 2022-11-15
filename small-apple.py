ffab67d371d9eab62d40afc8671d81402c4256d6
CAS <78571139+CAS-14@users.noreply.github.com>
2022-11-15 10:47:21 -0500

    shorten even more!!!

--- small-apple.py
+++ small-apple.py
@@ -1,6 +1,6 @@
-import os,PIL.Image,playsound,threading,time
+import os,PIL.Image as i,playsound as s,threading as t,time
 d,p=os.get_terminal_size(),";ffmpeg -i v.webm "
 os.system(f"yt-dlp -o v youtu.be/FtutLA63Cp8{p}a.mp3{p}-filter:v fps=30 %4d.png")
-o=["".join([("#"if p>9 else" ")for p in PIL.Image.open(f).resize((d.columns,d.lines-1)).convert("RGB").getdata(0)])for f in sorted(os.listdir())[:-2]]
-threading.Thread(target=playsound.playsound,args=["a.mp3"],daemon=True).start()
+o=["".join([("#"if p>9 else" ")for p in i.open(f).resize((d.columns,d.lines-1)).convert("RGB").getdata(0)])for f in sorted(os.listdir())[:-2]]
+t.Thread(target=s.playsound,args=["a.mp3"],daemon=True).start()
 for p in o:print(p);time.sleep(1/30)
