341d5efdd7e693accee0659c0773c7f730ae8911
CAS <78571139+CAS-14@users.noreply.github.com>
2022-11-15 10:41:21 -0500

    make it even shorter

--- small-apple.py
+++ small-apple.py
@@ -1,5 +1,5 @@
 import os,PIL.Image,playsound,threading,time
-d,p=os.get_terminal_size(),"&&ffmpeg -i v.webm "
+d,p=os.get_terminal_size(),";ffmpeg -i v.webm "
 os.system(f"yt-dlp -o v youtu.be/FtutLA63Cp8{p}a.mp3{p}-filter:v fps=30 %4d.png")
 o=["".join([("#"if p>9 else" ")for p in PIL.Image.open(f).resize((d.columns,d.lines-1)).convert("RGB").getdata(0)])for f in sorted(os.listdir())[:-2]]
 threading.Thread(target=playsound.playsound,args=["a.mp3"],daemon=True).start()
